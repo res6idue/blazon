d474b406ac8e7daad856578cd7eca67f059b28df
soon <kento.soon@gmail.com>
2023-01-25 04:21:57 +0900

    fix unable to update bug i

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
-  get 'relationship/create', to: 'relationship#create', as: 'relationship_create'
-  get 'users/profile/:id', to: 'users#edit', as: 'users_edit'
-  put 'users/:id', to: 'users#update', as: 'users_update'
-  get 'users', to: 'users#index', as: 'users_index'
-  get 'likes/:id', to: 'blazos#like', as: 'blazo_like'
-  get 'relationships/:id', to: 'users#follow', as: 'user_follow'
+  get "relationship/create", to: "relationship#create", as: "relationship_create"
+  get "users/profile/:id", to: "users#edit", as: "users_edit"
+  put "users/profile/:id", to: "users#update", as: "users_update"
+  get "users", to: "users#index", as: "users_index"
+  get "likes/:id", to: "blazos#like", as: "blazo_like"
+  get "relationships/:id", to: "users#follow", as: "user_follow"
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
   resources :blazos
-  resources :users, only: [:update, :destroy, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
