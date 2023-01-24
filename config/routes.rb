b2da1a6398ba84391e4c3ca8e01548d7630aa608
soon <kento.soon@gmail.com>
2023-01-25 02:12:14 +0900

    user now working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'relationship/create', to: 'relationship#create', as: 'relationship_create'
-  get 'users/profile/:id', to: 'users#edit', as: 'users_edit'
-  put 'users/profile/:id', to: 'users#update', as: 'users_update'
+  get 'users/profile:id', to: 'users#edit', as: 'users_edit'
+  put 'users/:id', to: 'users#update', as: 'users_update'
   get 'likes/create/:id', to: 'blazos#like', as: 'blazo_like'
 
   devise_for :users, controllers: {
