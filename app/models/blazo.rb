f97ff3b4a573ece4d151de74d181e34310f1ee98
soon <kento.soon@gmail.com>
2023-01-25 18:45:09 +0900

    restrict posts

--- app/models/blazo.rb
+++ app/models/blazo.rb
@@ -1,4 +1,7 @@
 class Blazo < ApplicationRecord
   belongs_to :user
   has_many :likes
+  # https://qiita.com/miriwo/items/9d93b8e7083df03f8809 を参考にした
+  validates :abstract, {presence: true, length: {maximum: 200}}
+  validates :content, {presence: true}
 end
