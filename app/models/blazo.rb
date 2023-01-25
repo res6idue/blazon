ddf012d39e6b6a03509397aa7b3ccebf9c0b6cb2
soon <kento.soon@gmail.com>
2023-01-26 00:32:33 +0900

    allow blank content

--- app/models/blazo.rb
+++ app/models/blazo.rb
@@ -3,5 +3,4 @@ class Blazo < ApplicationRecord
   has_many :likes
   # https://qiita.com/miriwo/items/9d93b8e7083df03f8809 を参考にした
   validates :abstract, {presence: true, length: {maximum: 200}}
-  validates :content, {presence: true}
 end
